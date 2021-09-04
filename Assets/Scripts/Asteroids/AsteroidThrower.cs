76ef9f13f059bffb02a60f36513770a0c296da65
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2021-09-04 19:17:34 +0200

    Code cleanup

--- Assets/Scripts/Asteroids/AsteroidThrower.cs
+++ Assets/Scripts/Asteroids/AsteroidThrower.cs
@@ -1,8 +1,10 @@
-﻿using UnityEditor;
+﻿//@Author: Teodor Tysklind / FutureGames / Teodor.Tysklind@FutureGames.nu
+
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class MeteorManager : MonoBehaviour
+public class AsteroidThrower : MonoBehaviour
 {
     [Header("Spawn points")]
     [SerializeField] private float boundsSizeX;
@@ -18,43 +20,50 @@ public class MeteorManager : MonoBehaviour
     private Vector3[] positions;
     private float timer;
     private AsteroidManager asteroidManager;
+    private Vector3 centerPosition;
     
     private void OnValidate()
     {
+        centerPosition = transform.position;
+        
         noOfPointsX = (int) (boundsSizeX / resolution);
         int noOfPointsY = (int) (boundsSizeY / resolution);
         positions = new Vector3[noOfPointsX*2 + noOfPointsY*2];
         
-        positions[0] = new Vector3(-boundsSizeX/2, -boundsSizeY/2) + transform.position;
-        positions[noOfPointsX] = new Vector3(boundsSizeX/2, -boundsSizeY/2)+ transform.position;
-        positions[noOfPointsX*2] = new Vector3(-boundsSizeX/2, boundsSizeY/2)+ transform.position;
-        positions[noOfPointsX*2+noOfPointsY] = new Vector3(boundsSizeX/2, boundsSizeY/2)+ transform.position;
+        positions[0] = new Vector3(-boundsSizeX/2, -boundsSizeY/2) + centerPosition;
+        positions[noOfPointsX] = new Vector3(boundsSizeX/2, -boundsSizeY/2)+ centerPosition;
+        positions[noOfPointsX*2] = new Vector3(-boundsSizeX/2, boundsSizeY/2)+ centerPosition;
+        positions[noOfPointsX*2+noOfPointsY] = new Vector3(boundsSizeX/2, boundsSizeY/2)+ centerPosition;
       
         for (int i = 1; i < noOfPointsX; i++)
         {
-            positions[i] = new Vector3(-boundsSizeX/2 + i * resolution, boundsSizeY/2, 0)+ transform.position;
-            positions[i+noOfPointsX] = new Vector3(-boundsSizeX/2 + i * resolution, -boundsSizeY/2, 0)+ transform.position;
+            positions[i] = new Vector3(-boundsSizeX/2 + i * resolution, boundsSizeY/2, 0)+ centerPosition;
+            positions[i+noOfPointsX] = new Vector3(-boundsSizeX/2 + i * resolution, -boundsSizeY/2, 0)+ centerPosition;
         }
 
         for (int i = 1; i < noOfPointsY; i++)
         {
             positions[i+noOfPointsX*2] = new Vector3(-boundsSizeX/2 , -boundsSizeY/2+  i * resolution, 0)+ transform.position;
-            positions[i+noOfPointsX*2 + noOfPointsY] = new Vector3(boundsSizeX/2 , -boundsSizeY/2+  i * resolution, 0)+ transform.position;
+            positions[i+noOfPointsX*2 + noOfPointsY] = new Vector3(boundsSizeX/2 , -boundsSizeY/2+  i * resolution, 0)+ centerPosition;
         }
     }
+    
 #if UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        Vector3 position1 = new Vector3(-boundsSizeX/2, -boundsSizeY/2, 0)+ transform.position;
-        Vector3 position2 = new Vector3(boundsSizeX/2, -boundsSizeY/2, 0)+ transform.position;
-        Vector3 position3 = new Vector3(-boundsSizeX/2, boundsSizeY/2, 0)+ transform.position;
-        Vector3 position4 = new Vector3(boundsSizeX/2, boundsSizeY/2, 0)+ transform.position;
+        Vector3 position1 = new Vector3(-boundsSizeX/2, -boundsSizeY/2, 0)+ centerPosition;
+        Vector3 position2 = new Vector3(boundsSizeX/2, -boundsSizeY/2, 0)+ centerPosition;
+        Vector3 position3 = new Vector3(-boundsSizeX/2, boundsSizeY/2, 0)+ centerPosition;
+        Vector3 position4 = new Vector3(boundsSizeX/2, boundsSizeY/2, 0)+ centerPosition;
         
+        Gizmos.color = Color.yellow;
+
         foreach (Vector3 position in positions)
         {
-            Gizmos.DrawSphere(position, 1f);
+            Gizmos.DrawSphere(position, 3f);
         }
         
+        Handles.color = Color.yellow;
         Handles.DrawLine(position1, position2);
         Handles.DrawLine(position3, position4);
         Handles.DrawLine(position1, position3);
@@ -75,13 +84,12 @@ public class MeteorManager : MonoBehaviour
 
         if (timer == 0)
         {
-            ThrowMeteor();
+            ThrowAsteroid();
             timer += intervalInFrames;
         }
-        
     }
 
-    private void ThrowMeteor()
+    private void ThrowAsteroid()
     {
         Vector3 position = positions[Random.Range(0, positions.Length)];
         Vector3 velocity = (Vector3.zero - position).normalized * magnitude;
